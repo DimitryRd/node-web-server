fce34a0c69a8c5b3f27adc2947680b2507453767
DimitryRd <dimitry.rd@gmail.com>
2018-08-02 22:51:40 +0300

    Logger added and loremIpsum added

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require('express');
-const hbs = require('hbs')
+const hbs = require('hbs');
+const loremIpsum = require('lorem-ipsum'), output = loremIpsum();
+const fs = require('fs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + "/views/partials");
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+
+app.use((req, res, next) =>{
+   var now = new Date().toString();
+   var log = `${now}: ${req.method} ${req.url}`
+   console.log(log);
+   fs.appendFile('server.log', log + '\n', (err) =>{
+       if(err){
+           console.log('Unable to append to server.log')
+       }
+   });
+    next();
+});
 
 app.get('/', (req, res) => {
     res.render('home.hbs', {
@@ -14,16 +30,29 @@ app.get('/', (req, res) => {
             'Biking',
             'Cities'
         ],
-        year: new Date().getFullYear()
+        year: new Date().getFullYear(),
+        text: output
+
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
+        currentYear: new Date().getFullYear(),
+        text: output
     })
 });
+
+app.get('/maintenance', (req, res) => {
+    res.render('maintenance.hbs', {
+        pageTitle: 'Maintenance',
+        currentYear: new Date().getFullYear(),
+        text: output
+    })
+});
+
+
 
 app.get('/bad', (req, res) => {
     res.send({
